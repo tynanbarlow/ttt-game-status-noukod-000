2566c33e0b6c89bc9675db8ff21923cad3ba8c53
Tynan Barlow <tynan@noukod.org>
2018-09-25 12:39:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
+    puts "---"
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
     else
